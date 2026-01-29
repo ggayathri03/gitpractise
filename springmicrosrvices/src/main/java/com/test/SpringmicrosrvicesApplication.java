c984b2a75c8038ea1546bce5a8c64e398777466b
ggayathri03 <156501818+ggayathri03@users.noreply.github.com>
2026-01-29 14:22:44 +0530

    Add files via upload

---
+++ springmicrosrvices/src/main/java/com/test/SpringmicrosrvicesApplication.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmicrosrvicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmicrosrvicesApplication.class, args);
+	}
+
+}
+            
