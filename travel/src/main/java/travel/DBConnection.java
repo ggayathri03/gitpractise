c85b386d8b8b85b6064b91a8d44bbceda19584da
ggayathri03 <156501818+ggayathri03@users.noreply.github.com>
2026-01-29 14:22:10 +0530

    Add files via upload

---
+++ travel/src/main/java/travel/DBConnection.java
@@ -0,0 +1,23 @@
+package travel;
+import java.sql.Connection;
+import java.sql.DriverManager;
+ 
+public class DBConnection {
+ 
+	static String url="jdbc:mysql://localhost:3306/travelapp";
+	static String user="root";
+	static String pass="root@39";
+ 
+	public static Connection getConnection() {
+		Connection con=null;
+ 
+		try {
+			con=DriverManager.getConnection(url,user,pass);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+ 
+}
