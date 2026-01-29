36f03574f4e78c965f037715035af6c3fc5267d9
ggayathri03 <156501818+ggayathri03@users.noreply.github.com>
2026-01-29 14:31:48 +0530

    Add files via upload

---
+++ ServletExp1/src/main/java/com/test/LifecycleExp.java
@@ -0,0 +1,31 @@
+package com.test;
+
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+
+
+@WebServlet("/LifecycleExp")
+public class LifecycleExp extends GenericServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init()");
+	}
+
+	
+	public void destroy() {
+		System.out.println("destroy()");
+	}
+
+	
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		System.out.println("service() process reqeust and response");
+	}
+}
